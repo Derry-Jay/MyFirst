d11b79991fbf995b81b0d4c53f7708aa71f189f8
Derry432 <derick.j@sagous.in>
2023-02-22 22:54:23 +0530

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by Derrick on 22/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
